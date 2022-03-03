bb82277ef8f4d10567cd1614616440778adf7b8d
Matthäus Winkler <87285710+Matthaeus07@users.noreply.github.com>
2022-03-04 00:31:36 +0100

    Update Linear Function Calculator.py

--- Linear Function Calculator.py
+++ Linear Function Calculator.py
@@ -1,10 +1,42 @@
-print("This tool was made to give a term representation of f, using two known points from the graf of a linear function f. Eg.: (Px | Py), (Qx | Qy)\n\n")
-
-
 def calc():
 
+    # Choose the operation
+    choose = input("""If you want a term representation of f as an output using two known points from the graf of a linear function f
+  Eg.: (Px | Py), (Qx | Qy)
+press ENTER.
+
+If you want to calculate the zero point using the term of f
+type 'z' and press ENTER.
+""")
+
+    # Getting the zero point
+    def get_zero():
+        print(f"f(x) = {k} * x + {d} = 0\n\n0 = {k} * x + {d}   | -{d}\n{0 - d} = {k} * x   | :{k}\n{0 - d} / {k} = {(0 - d) / k} = x\n\nN ({(0 - d) / k} | 0)")
+        again = input(
+            "\n\n\n\n- If you want to restart press ENTER.\n- If you want to quit type 'e' and press ENTER.\n")
+        if again == "e":
+            exit()
+        else:
+            print("\n\n\n\n")
+            calc()
+
+    # Input Variables for zero point
+    if choose == "z":
+        k = float(input("\nk: "))
+        d = float(input("d: "))
+
+        check = input(
+            f"\nIf those Variables are right press ENTER, else type 'r' and press ENTER to restart.:\nf(x) = {k} * x + {d}\n")
+        if check == "r":
+            print("\n\n\n\n")
+            calc()
+        else:
+            print("\n\nSolution:\n")
+            get_zero()
+
+
     # Input Variables
-    Px = float(input("Px: "))
+    Px = float(input("\nPx: "))
     Py = float(input("Py: "))
 
     Qx = float(input("Qx: "))
@@ -28,17 +60,6 @@ def calc():
 
     print(f"{Py} = {k} * {Px} + d\nd = {Py} - {k} * {Px}\nd = {d}\n\n")
 
-    # Getting the zero point
-    def get_zero():
-        print(f"f(x) = {k} * x + {d} = 0\n\n0 = {k} * x + {d}   | -{d}\n{0 - d} = {k} * x   | :{k}\n{0 - d} / {k} = {(0 - d) / k} = x\n\nN ({(0 - d) / k} | 0)")
-        again = input(
-            "\n\n\n\n- If you want to restart press ENTER.\n- If you want to quit type 'e' and press ENTER.\n")
-        if again == "e":
-            exit()
-        else:
-            print("\n\n\n\n")
-            calc()
-
 
     # Term representation
     print(f"f(x) = {k} * x + {d}")
@@ -54,6 +75,8 @@ def calc():
         print("\n\n\n\n")
         calc()
 
+
+    
 
 
 calc()
